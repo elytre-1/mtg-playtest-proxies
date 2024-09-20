037c50806840f8df90481d0c61d02d93fd935f12
Elytra <a.p.f.reynaud@gmail.com>
2024-09-20 15:29:39 +0200

    minor changes + readme file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mtg-playtest-proxies
+This project is an attempt to generate a single file, cheap to print and decent looking playtest proxy for MTG cards. It is based on the Scryfall API, gets the card images, automatically change their brightness, and redraw the card in a custom layout. The user will be able to print their proxies from a .pdf file.
+
+## Workflow
+1. Import your card list in  `./user/`. The format is  `[number of copies] [card name]`. Edit the `card_list_path` variable if necessary.
+2. Run `python ./script/main.py`
+3. Get your .pdf file in `./output/`
+
+## Dependencies
+Dependencies include the following python libraries:
+- requests
+- json
+- shutil
+- cv2
+- PIL
+- numpy
+- textwrap
+- re
+- PyPDF2
--- scripts/main.py
+++ scripts/main.py
@@ -1,21 +1,13 @@
-# Import required methods
-from requests import get
-from json import loads
-from shutil import copyfileobj
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw 
-import numpy as np
-import cv2
-import textwrap
-from matplotlib import font_manager
 import func
 
 # TODO handle double-side cards
+# TODO a function for altered proxies
+# TODO handle number of copies
 
 if __name__ == '__main__':
     # Get for your deck
-    list_of_cards, number_of_copies = func.getListOfCards(path='../user/card_list.txt')
+    card_list_path = '../user/card_list.txt'
+    list_of_cards, number_of_copies = func.getListOfCards(path=card_list_path)
 
     # Create a .png for every card
     for copies, card_name in zip(number_of_copies, list_of_cards):
@@ -33,5 +25,3 @@ if __name__ == '__main__':
 
     # Create pages to print
     func.A4layout(list_of_cards)
-
-    # TODO A function for altered proxies
